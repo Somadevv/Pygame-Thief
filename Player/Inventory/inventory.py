018d1c137e6639cff5814623f1616e6c90300d0f
Josh <josh@insitesdigital.com>
2022-12-09 22:15:13 +0000

    Added inventory bag to screen

--- Player/Inventory/inventory.py
+++ Player/Inventory/inventory.py
@@ -1,2 +1,22 @@
+import pygame
+import Helpers.drawText
+
+DrawText = Helpers.drawText.DrawText
+
+
 class Inventory():
-  pass
+    def __init__(self):
+        self.BAG_COLOR = (125, 22, 55)
+        self.BAG_HEIGHT = 65
+        self.BAG_WIDTH = 65
+        self.BAG_XPOS = 20
+        self.BAG_YPOS = 350
+
+    def DrawInventoryBagToWindow(self, surface):
+        BAG_ICON = pygame.image.load("Assets/Images/Icons/bag.png").convert()
+        TEXT_SIZE = 20
+        image = pygame.transform.scale(
+            BAG_ICON, (self.BAG_WIDTH, self.BAG_HEIGHT))
+        surface.blit(image, (self.BAG_XPOS, self.BAG_YPOS))
+        DrawText(surface, "Tab", TEXT_SIZE, (255, 255, 255),
+                 50, 385)
--- game.py
+++ game.py
@@ -1,32 +1,36 @@
-import pygame, sys
+import pygame
+import sys
+import Player.Inventory.inventory
+import Helpers.drawText
+
+
 pygame.init()
+
+playerInventory = Player.Inventory.inventory.Inventory()
+DrawText = Helpers.drawText.DrawText
+
 
 # Global variables
 CLOCK = pygame.time.Clock()
 GAME_TICKRATE = 120
-SCREEN_WIDTH = 750
-SCREEN_HEIGHT = 500
-
-# Colors
-COLOR_WHITE = 255, 255, 255
-
-GAME_WINDOW = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+GAME_WIDTH = 750
+GAME_HEIGHT = 500
+GAME_WINDOW = pygame.display.set_mode((GAME_WIDTH, GAME_HEIGHT))
 
 # Game loop
 while True:
     # Exit
     for event in pygame.event.get():
-        if event.type == pygame.QUIT : sys.exit()
+        if event.type == pygame.QUIT:
+            sys.exit()
 
-    # Fill the background with white
-    GAME_WINDOW.fill((COLOR_WHITE))
+    # Fill background (BLACK)
+    GAME_WINDOW.fill((0, 0, 0))
 
-    # Draw a solid blue circle in the center
-    pygame.draw.circle(GAME_WINDOW, (0, 0, 255), (250, 250), 75)
-
-    #This is from harry
+    # Draw Player inventory bag
+    playerInventory.DrawInventoryBagToWindow(
+        GAME_WINDOW)
 
     # Flip the display
     pygame.display.update()
     CLOCK.tick(GAME_TICKRATE)
-
